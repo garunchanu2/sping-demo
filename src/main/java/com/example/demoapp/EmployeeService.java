dfded6d1f32a98306608472e19d48b9ffc6ea108
garun.c <garun.c@kbtg.tech>
2021-09-07 10:30:49 +0700

    Integret repository to service

--- src/main/java/com/example/demoapp/EmployeeService.java
+++ src/main/java/com/example/demoapp/EmployeeService.java
@@ -1,14 +1,30 @@
 package com.example.demoapp;
 
+import com.example.demoapp.employees.Employee;
+import com.example.demoapp.employees.EmployeeRepository;
 import com.example.demoapp.employees.EmployeeResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
     public EmployeeResponse getById(int id) {
-        EmployeeResponse employeeResponse01 = new EmployeeResponse();
-        employeeResponse01.setId(id);
-        employeeResponse01.setName("Garun");
-        return employeeResponse01;
+//        EmployeeResponse employeeResponse01 = new EmployeeResponse();
+//        employeeResponse01.setId(id);
+//        employeeResponse01.setName("Garun");
+        Optional<Employee> result = employeeRepository.findById(id);
+        if(result.isPresent()) {
+            EmployeeResponse response = new EmployeeResponse();
+            response.setId(result.get().getId());
+            response.setName(result.get().getName());
+            return response;
+        }
+        throw new RuntimeException("Employee not found id = " + id);
     }
 }
