12aa2790e90f89fa73b8000070f27577df8997ca
garun.c <garun.c@kbtg.tech>
2021-09-07 10:04:53 +0700

    Testing Mock data JPA

--- src/main/java/com/example/demoapp/employees/EmployeeRepository.java
+++ src/main/java/com/example/demoapp/employees/EmployeeRepository.java
@@ -2,5 +2,12 @@ package com.example.demoapp.employees;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+//    List<Employee> findAllByIdAndName(int id, String name);
+//    Select * from Employee where id = ? and name = ?
+
+
 }
---
+++ src/test/java/com/example/demoapp/employees/EmployeeRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.example.demoapp.employees;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+//@Import(EmployeeRepository.class)
+public class EmployeeRepositoryTest {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Test
+    @DisplayName("ค้นหาข้อมูลสำเร็จ ID = 1")
+    public void case01() {
+        //Arrange
+        Employee employee1 = new Employee();
+        employee1.setName("Mock name");
+        employeeRepository.save(employee1);
+        //Act
+        Optional<Employee> result = employeeRepository.findById(1);
+//        if(result.isPresent()) => check != null
+        //Assert
+        assertTrue(result.isPresent());
+        assertEquals(1, result.get().getId());
+        assertEquals("Mock name", result.get().getName());
+    }
+}
